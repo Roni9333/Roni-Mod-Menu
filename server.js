8d96849c5c1e6b2888e0f92089f16ba3f5f0f2ba
Roni9333 <businessroni93@gmail.com>
2025-10-29 08:43:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const cors = require("cors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
+
 const KEYS_FILE = path.join(__dirname, "keys.json");
-
-// Change this password for production
 const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || "roni6294";
-
 // load keys
 let keys = [];
 function loadKeys() {
